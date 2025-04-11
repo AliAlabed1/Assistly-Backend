42ea5b2d4e2bf693de0dded1a329bddf108c6483
ALIALABED1 <alialabed161@gmail.com>
2025-04-11 18:41:15 +0300

    add CORS_ALLOWED_ORIGINS

--- assistly/settings.py
+++ assistly/settings.py
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'assistly.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -128,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
